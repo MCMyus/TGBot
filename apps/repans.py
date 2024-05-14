62e3a103eaa4bf34e7f08b341d12d7c34b5c6b0d
Bill Mus <bill.mus@icloud.com>
2024-05-14 14:47:26 +0300

    commit

--- apps/repans.py
+++ apps/repans.py
@@ -1,12 +1,6 @@
 from aiogram import types
 
-
 rep_ans_markup = types.InlineKeyboardMarkup(resize_keyboard=True)
-<<<<<<< HEAD
 ans = types.InlineKeyboardButton(text='✏️ Ответить', callback_data='ans')
 ign = types.InlineKeyboardButton(text='🗑️ Удалить', callback_data='ign')
-=======
-ans = types.InlineKeyboardButton(text='Ответить', callback_data=f'ans')
-ign = types.InlineKeyboardButton(text='Удалить', callback_data='ign')
->>>>>>> origin/master
 rep_ans_markup.add(ans, ign)
